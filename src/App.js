415a62ed9bdddec8f7c3d47434e2ccd8fd5d9211
behshad <behshadrad04@gmail.com>
2022-07-23 02:01:41 +0430

    finish

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import StopWatch from "./components/stopWatch/StopWatch";
+import Gallery from "./components/gallery/Gallery";
+
+function App() {
+  return (
+    <div className="App">
+     {/*  STOPWATCH project */}
+     {/*<StopWatch/>*/}
+
+    {/* UNSPLASH GALLERY project*/}
+    <Gallery/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/stopWatch/StopWatch.jsx
@@ -0,0 +1,44 @@
+import React, {useEffect, useState} from "react"
+import Buttons from "../Buttons/Buttons";
+import Timer from "../Timer/Timer";
+
+const StopWatch = () => {
+    const [timer, setTimer] = useState(0)
+    const [pause, setPause] = useState(false)
+    const [active, setActive] = useState(false)
+
+    useEffect(() => {
+        let interval;
+        if (!pause && active) {
+            interval = setInterval(() => {
+                setTimer(prevState => prevState + 10)
+            }, 10)
+        } else {
+            clearInterval(interval)
+        }
+        return () => clearInterval(interval)
+    }, [active, pause])
+
+    const handleStop = () => {
+        setActive(false)
+        setPause(false)
+        setTimer(0)
+    }
+
+    const handlePause = () => {
+        setPause(prev => !prev)
+    }
+
+    return (
+        <div className={`stopwatch`}>
+            <Timer timer={timer}/>
+            <Buttons active={active}
+                     setActive={setActive}
+                     pause={pause}
+                     pauseHandler={handlePause}
+                     stopHandler={handleStop}/>
+        </div>
+    )
+}
+
+export default StopWatch
